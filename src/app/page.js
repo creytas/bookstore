208746c15cacfd84456a0b90145f523c0f59f3e5
creytas <creytas@gmail.com>
2023-11-30 01:15:03 +0100

    feat: add header component to layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Bookstore</h1>
+    <main className="w-screen min-h-screen">
+      <h1 className="w-1/4 flex items-center space-x-2 border border-red-600">
+        <FontAwesomeIcon icon={faBookmark} />
+        <span>Bookstore</span>
+      </h1>
     </main>
-  )
+  );
 }

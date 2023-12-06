b54a5d5548ea331b4f421e8f84673f6f7bcfed77
creytas <creytas@gmail.com>
2023-12-07 00:08:03 +0100

    refactor: read and render data

--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,55 @@
-import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
-import Ratings from "@/components/card/ratings";
 import Card from "@/components/card/card";
 import Booklist from "@/components/list/booklist";
 
 export default function Home() {
+  const books =[
+    {
+      image:"/assets/bescherelle_cover.jpg",
+      title:"Bescherelle Francais",
+      price:"10",
+      rating:null
+    },
+    {
+      image:"/assets/fireworks_cover.png",
+      title:"Fireworks",
+      price:"1",
+      rating:1
+    },
+    {
+      image:"/assets/math_cover.jpg",
+      title:"Maths 6",
+      price:"100,000",
+      rating:2
+    },
+    {
+      image:"/assets/physique_cover.jpeg",
+      title:"Physique Chimie 2",
+      price:"50,000",
+      rating:3
+    },
+    {
+      image:"/assets/histoire_cover.jpg",
+      title:"Histoire 1",
+      price:"500",
+      rating:4
+    },
+    {
+      image:"",
+      title:"Le Robert college",
+      price:"2,300",
+      rating:5
+    }
+  ]
   return (
     <main className="w-screen min-h-screen">
       <Booklist titleIcon={faBookmark} title="best-seller">
-        <Card image="/assets/bescherelle_cover.jpg" title="Bescherelle Francais" price="10" />
-        <Card image="/assets/fireworks_cover.png" title="Fireworks" price="0" rating={1} />
-        <Card image="/assets/math_cover.jpg" title="Maths 6" price="100,000" rating={2} />
-        <Card image="/assets/physique_cover.jpeg" title="Physique Chimie 2" price="50,000" rating={3} />
-        <Card image="/assets/histoire_cover.jpg" title="Histoire 1" price="500" rating={4} />
-        <Card title="Le Robert college" price="2,300" rating={5} />
+        {
+          books.map((book, index)=>{
+            return <Card key={index} data={book} />
+          })
+        }
       </Booklist>
-
-      <Booklist titleIcon={faBookmark} title="upcoming">
-        <Card image="/assets/bescherelle_cover.jpg" title="Bescherelle Francais" price="10" />
-        <Card image="/assets/fireworks_cover.png" title="Fireworks" price="0" rating={1} />
-        <Card image="/assets/math_cover.jpg" title="Maths 6" price="100,000" rating={2} />
-        <Card image="/assets/physique_cover.jpeg" title="Physique Chimie 2" price="50,000" rating={3} />
-        <Card image="/assets/histoire_cover.jpg" title="Histoire 1" price="500" rating={4} />
-        <Card title="Le Robert college" price="2,300" rating={5} />
-      </Booklist>
-
-      <Booklist titleIcon={faBookmark} title="low cost">
-        <Card image="/assets/bescherelle_cover.jpg" title="Bescherelle Francais" price="10" />
-        <Card image="/assets/fireworks_cover.png" title="Fireworks" price="0" rating={1} />
-        <Card image="/assets/math_cover.jpg" title="Maths 6" price="100,000" rating={2} />
-        <Card image="/assets/physique_cover.jpeg" title="Physique Chimie 2" price="50,000" rating={3} />
-        <Card image="/assets/histoire_cover.jpg" title="Histoire 1" price="500" rating={4} />
-        <Card title="Le Robert college" price="2,300" rating={5} />
-      </Booklist>      
     </main>
   );
 }

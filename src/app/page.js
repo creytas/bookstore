5cb1793222d03368137a23c75053c273895908b9
creytas <creytas@gmail.com>
2023-12-06 03:34:04 +0100

    refactor: stylize and show list component

--- src/app/page.js
+++ src/app/page.js
@@ -3,23 +3,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 import Ratings from "@/components/card/ratings";
 import Card from "@/components/card/card";
+import Booklist from "@/components/list/booklist";
 
 export default function Home() {
   return (
     <main className="w-screen min-h-screen">
-      <h1 className="w-full flex items-center space-x-2 px-4 border border-red-600 bg-white">
-        <FontAwesomeIcon icon={faBookmark} className="text-blue-900" />
-        <span className="text-blue-900">Bookstore</span>
-        <Ratings />
-      </h1>
-      <div className="grid grid-cols-6 gap-3 px-4 bg-white">
+      <Booklist titleIcon={faBookmark} title="best-seller">
         <Card image="/assets/bescherelle_cover.jpg" title="Bescherelle Francais" price="10" />
         <Card image="/assets/fireworks_cover.png" title="Fireworks" price="0" rating={1} />
         <Card image="/assets/math_cover.jpg" title="Maths 6" price="100,000" rating={2} />
         <Card image="/assets/physique_cover.jpeg" title="Physique Chimie 2" price="50,000" rating={3} />
         <Card image="/assets/histoire_cover.jpg" title="Histoire 1" price="500" rating={4} />
         <Card title="Le Robert college" price="2,300" rating={5} />
-      </div>
+      </Booklist>
+
+      <Booklist titleIcon={faBookmark} title="upcoming">
+        <Card image="/assets/bescherelle_cover.jpg" title="Bescherelle Francais" price="10" />
+        <Card image="/assets/fireworks_cover.png" title="Fireworks" price="0" rating={1} />
+        <Card image="/assets/math_cover.jpg" title="Maths 6" price="100,000" rating={2} />
+        <Card image="/assets/physique_cover.jpeg" title="Physique Chimie 2" price="50,000" rating={3} />
+        <Card image="/assets/histoire_cover.jpg" title="Histoire 1" price="500" rating={4} />
+        <Card title="Le Robert college" price="2,300" rating={5} />
+      </Booklist>
+
+      <Booklist titleIcon={faBookmark} title="low cost">
+        <Card image="/assets/bescherelle_cover.jpg" title="Bescherelle Francais" price="10" />
+        <Card image="/assets/fireworks_cover.png" title="Fireworks" price="0" rating={1} />
+        <Card image="/assets/math_cover.jpg" title="Maths 6" price="100,000" rating={2} />
+        <Card image="/assets/physique_cover.jpeg" title="Physique Chimie 2" price="50,000" rating={3} />
+        <Card image="/assets/histoire_cover.jpg" title="Histoire 1" price="500" rating={4} />
+        <Card title="Le Robert college" price="2,300" rating={5} />
+      </Booklist>      
     </main>
   );
 }

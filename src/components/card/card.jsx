d7b8f725be84c9bf95d30810240c43e1f7f4c93a
creytas <creytas@gmail.com>
2023-12-07 00:06:18 +0100

    refactor: managing data flow within the component

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,16 +4,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import Ratings from "./ratings";
 
-const Card = ({ image, title, price, rating }) => {
-  const handle = () => console.log("add to cart clicked");
+const Card = ({ data }) => {
+  const { image, title, price, rating } = data;
+  const handle = ()=>console.log(`${title} added to cart`);
+
   return (
     <div className="rounded-md border border-gray-300 p-2 my-4 cursor-pointer hover:shadow-md hover:shadow-gray-900">
       <div className="w-full h-40 mb-3 relative">
-        <Image src={image ? image : "/assets/bookstore.png"} alt={title} fill className="object-cover" />
+        <Image
+          src={image ? image : "/assets/bookstore.png"}
+          alt={title}
+          fill
+          className="object-cover"
+        />
       </div>
       <h2 className="text-gray-900 text-lg font-semibold">{title}</h2>
       <div className="flex space-x-2">
-        <Ratings ratingValue={rating} readOnly={true} />
+        <Ratings ratingValue={rating ? rating : 0} readOnly={true} />
         <h3 className="text-gray-500 text-sm font-medium">{`CDF ${price}`}</h3>
       </div>
       <button

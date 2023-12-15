7eb50aaa4dc4b52ca147c60a95853d8dbfdc2f2c
creytas <creytas@gmail.com>
2023-12-15 21:57:14 +0100

    refactor: reframe data hydratation

--- src/app/page.js
+++ src/app/page.js
@@ -1,55 +1,36 @@
+"use client";
+import { useState, useEffect } from "react";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
-import Card from "@/components/card/card";
 import Booklist from "@/components/list/booklist";
+import Header from "@/components/navbar/header";
+import { books } from "@/lib/data";
 
 export default function Home() {
-  const books =[
-    {
-      image:"/assets/bescherelle_cover.jpg",
-      title:"Bescherelle Francais",
-      price:"10",
-      rating:null
-    },
-    {
-      image:"/assets/fireworks_cover.png",
-      title:"Fireworks",
-      price:"1",
-      rating:1
-    },
-    {
-      image:"/assets/math_cover.jpg",
-      title:"Maths 6",
-      price:"100,000",
-      rating:2
-    },
-    {
-      image:"/assets/physique_cover.jpeg",
-      title:"Physique Chimie 2",
-      price:"50,000",
-      rating:3
-    },
-    {
-      image:"/assets/histoire_cover.jpg",
-      title:"Histoire 1",
-      price:"500",
-      rating:4
-    },
-    {
-      image:"",
-      title:"Le Robert college",
-      price:"2,300",
-      rating:5
-    }
-  ]
+  const [data, setData] = useState(books);
+  const [filtered, setFiltered] = useState(books);
+  const [isFiltered, setIsFiltered] = useState(false);
+
+  const filterResult = (input) => {
+      let filteredData = data.flat().filter((item) => {
+      const title = item.title.toLowerCase();
+      const keyword = input.toLowerCase();
+      return title.indexOf(keyword) > -1;
+    });
+    setFiltered(filteredData);
+  };
+
+  useEffect(() => {
+    setData(data);
+  }, []);
+
   return (
     <main className="w-screen min-h-screen">
-      <Booklist titleIcon={faBookmark} title="best-seller">
-        {
-          books.map((book, index)=>{
-            return <Card key={index} data={book} />
-          })
-        }
-      </Booklist>
+      <Header filter={filterResult} setIsFiltered={setIsFiltered} />
+      <Booklist
+        titleIcon={faBookmark}
+        title="best-seller"
+        data={isFiltered ? filtered : data}
+      />
     </main>
   );
 }
--- src/components/list/booklist.jsx
+++ src/components/list/booklist.jsx
@@ -1,13 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Card from "@/components/card/card";
 
-const Booklist = ({ titleIcon, title, children }) => {
+const Booklist = ({ titleIcon, title, data }) => {
   return (
     <section className="mt-8 pt-4 border-t border-gray-300 relative">
       <h2 className="w-1/5 flex items-center justify-center space-x-2 px-4 text-lg bg-orange-600 absolute top-[-15px] backdrop-filter backdrop-blur-sm bg-opacity-20 border border-orange-600">
         <FontAwesomeIcon icon={titleIcon} className=" text-gray-900" />
         <span className={`text-orange-600 font-black uppercase`}>{title}</span>
       </h2>
-      <div className="grid grid-cols-6 gap-3 px-4 bg-white">{children}</div>
+      <div className="grid grid-cols-6 gap-3 px-4 bg-white">
+        {data.map((book, index) => {
+          return <Card key={index} data={book} />;
+        })}
+      </div>
     </section>
   );
 };
